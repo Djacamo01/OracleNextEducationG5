a7eaa129a8cc826d0195d1103299cca0587c2fd4
Djacamo01 <DONALD.JACAMO22@est.unanleon.edu.ni>
2023-06-06 09:23:28 -0600

    creando e iniciando repositrio de todo lo hecho en oracle next education

---
+++ Curso 3 programacion/test.cs
@@ -0,0 +1,17 @@
+using System;
+
+partial class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("¡Hola, mundo!");
+
+        // Aquí puedes agregar el código adicional que desees ejecutar
+        Console.WriteLine("¡Hola, mundo 2!");
+       var x=Console.ReadLine();
+       Console.WriteLine(x+"  no se que dice aca");
+
+        Console.WriteLine("Presiona cualquier tecla para salir.");
+        Console.ReadKey();
+    }
+}
